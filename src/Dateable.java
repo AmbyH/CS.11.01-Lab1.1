968bd49b7022b5f495d9cf2452441b0181021886
AmbyH <kinghah2026@student.cis.edu.hk>
2023-08-31 14:32:20 +0800

    update 1.9.8

--- src/Dateable.java
+++ src/Dateable.java
@@ -6,6 +6,6 @@ public class Dateable {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your current age.");
         Integer age = scanner.nextInt();
-        System.out.println("The youngest person you can date is " + (age/2+7) + " years old.");
+        System.out.println(age + "-year olds should date somebody who is at least " + (age/2+7) + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -5,7 +5,7 @@ public class Pages {
         Scanner sCanner = new Scanner(System.in);
         System.out.println("Please enter your age.");
         Integer age = sCanner.nextInt();
-        System.out.println(age +  "-year olds should read at least " + (100-age) + " pages before giving up. ");
+        System.out.println(age +  "-year olds should read at least " + (100-age) + " pages before giving up on a book. ");
 
     }
 }
